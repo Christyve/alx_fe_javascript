8ef1122c5efa9800fba3e24c1e744b374b139339
Christy Ekpiken <christyekpixs99@gmail.com>
2025-06-24 13:54:27 +0100

    commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -28,7 +28,7 @@ function showRandomQuote() {
 }
 
 // Add a new quote dynamically
-function addQuote() {
+function createAddQuoteForm() {
   const quoteText = document.getElementById("newQuoteText").value.trim();
   const quoteCategory = document.getElementById("newQuoteCategory").value.trim();
 
